c5a6d50484c1f4169c805e81ae0b63d6cb4077a6
Dave Toerber <david.toerber@broncos.nfl.net>
2026-02-12 11:53:21 -0700

    remove other components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,4 @@
 import { Routes } from '@angular/router';
 import { LayoutOne } from './components/layout-one/layout-one';
-import { LayoutTwo } from './components/layout-two/layout-two';
-import { LayoutThree } from './components/layout-three/layout-three';
 
-export const routes: Routes = [
-  { path: 'layout1', component: LayoutOne },
-  { path: 'layout2', component: LayoutTwo },
-  { path: 'layout3', component: LayoutThree },
-  { path: '', redirectTo: 'layout1', pathMatch: 'full' },
-];
+export const routes: Routes = [{ path: '', component: LayoutOne }];
--- src/app/components/layout-two/layout-two.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-layout-two',
-  imports: [],
-  templateUrl: './layout-two.html',
-  styleUrl: './layout-two.scss',
-})
-export class LayoutTwo {}
